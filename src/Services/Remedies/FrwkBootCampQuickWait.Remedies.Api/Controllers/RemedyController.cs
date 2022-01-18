361fb0fb96eb3b42b0ae15f368438162fe9f0eca
fleal07 <fleal07@gmail.com>
2022-01-18 16:25:25 -0300

    Implementacao das estruturas basicas

---
+++ src/Services/Remedies/FrwkBootCampQuickWait.Remedies.Api/Controllers/RemedyController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FrwkBootCampQuickWait.Remedies.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RemedyController : ControllerBase
+    {
+    }
+}
---
+++ src/Services/Remedies/FrwkBootCampQuickWait.Remedies.Infrastructure/Repositories/RemedyRepository.cs
@@ -0,0 +1,8 @@
+﻿using FrwkBootCampQuickWait.Remedies.Domain.Interfaces;
+
+namespace FrwkBootCampQuickWait.Remedies.Infrastructure.Repository
+{
+    public class RemedyRepository : IRemedyRepository
+    {
+    }
+}
