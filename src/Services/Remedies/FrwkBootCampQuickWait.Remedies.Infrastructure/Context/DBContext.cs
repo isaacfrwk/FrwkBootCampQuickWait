d4d53802e856431b84fad84708f59ef4c6e13d76
fleal07 <fleal07@gmail.com>
2022-01-20 18:55:54 -0300

    Inclusao do Context

---
+++ src/Services/Remedies/FrwkBootCampQuickWait.Remedies.Infrastructure/Context/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using FrwkBootCampQuickWait.Remedies.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrwkBootCampQuickWait.Remedies.Infrastructure.Context
+{
+    public class DBContext : DbContext//, IDbContext
+    {
+
+        public DBContext()
+        {
+
+        }
+
+        public DBContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Remedy> Remedies { get; set; }
+
+
+    }
+}
