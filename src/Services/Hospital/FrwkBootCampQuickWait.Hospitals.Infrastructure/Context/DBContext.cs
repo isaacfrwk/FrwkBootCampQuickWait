04276da4f8c8bd0942a5c6e10e4fb4ee2796c13f
Raphael Nogueira <raphaelnogueira@frwk.com.br>
2022-01-20 11:57:29 -0300

    Estruturação do contexto de Hospitais

---
+++ src/Services/Hospital/FrwkBootCampQuickWait.Hospitals.Infrastructure/Context/DBContext.cs
@@ -0,0 +1,25 @@
+﻿using FrwkBootCampQuickWait.Hospitals.Domain.Contracts;
+using FrwkBootCampQuickWait.Hospitals.Domain.Entities;
+using FrwkBootCampQuickWait.Hospitals.Infrastructure.HospitalContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrwkBootCampQuickWait.Hospitals.Infrastructure.Context
+{
+    public class DBContext : DbContext, IDbContext
+    {
+        public DBContext()
+        {
+        }
+
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Hospital> Hospitals { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new HospitalMap());
+        }
+    }
+}
---
+++ src/Services/Hospital/FrwkBootCampQuickWait.Hospitals.Infrastructure/HospitalContext/HospitalMap.cs
@@ -0,0 +1,15 @@
+﻿using FrwkBootCampQuickWait.Hospitals.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FrwkBootCampQuickWait.Hospitals.Infrastructure.HospitalContext
+{
+    public class HospitalMap : IEntityTypeConfiguration<Hospital>
+    {
+        public void Configure(EntityTypeBuilder<Hospital> builder)
+        {
+            builder.ToTable("Hospital");
+            builder.HasKey(x => x.Id);
+        }
+    }
+}
